aa938e1ad3e0fb37d778bf7a17c2feb2343ae97e
Rmaz01 <144160948+Rmaz01@users.noreply.github.com>
2024-03-21 15:44:04 -0500

    ProgrammingAssignment1 Upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    double feetCount;
+    double meterCount = 15;
+    System.out.printf("%-10s%-10s%-10s%-10s\n%-10s", "Feet", "Meters", "Meters", "Feet", "----------------------------------------");
+    for (feetCount = 1; feetCount <= 10; feetCount++) {
+      System.out.printf("\n%-10.1f%-10.3f%-10.1f%-10.3f", feetCount, footToMeter(feetCount), meterCount += 5, meterToFoot(meterCount));   
+    }
+  }
+
+  public static double footToMeter(double foot) {
+    double meter;
+    meter = 0.305 * foot;
+    return meter;
+  }
+
+  public static double meterToFoot(double meter) {
+    double foot;
+    foot = 3.279 * meter;
+    return foot;
+  }
+}
