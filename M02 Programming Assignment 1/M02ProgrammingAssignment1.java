76ece95da9b87e8fa711fe38e1abae2bd634d90d
Rmaz01 <144160948+Rmaz01@users.noreply.github.com>
2024-03-30 04:24:52 -0500

    Add files via upload

---
+++ M02 Programming Assignment 1/M02ProgrammingAssignment1.java
@@ -0,0 +1,52 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+class MyDate {
+    private int year;
+    private int month;
+    private int day;
+
+    public MyDate() {
+        this(System.currentTimeMillis());
+    }
+
+    public MyDate(long elapsedTime) {
+        setDate(elapsedTime);
+    }
+
+    public MyDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDate(long elapsedTime) {
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTimeInMillis(elapsedTime);
+        this.year = calendar.get(Calendar.YEAR);
+        this.month = calendar.get(Calendar.MONTH);
+        this.day = calendar.get(Calendar.DAY_OF_MONTH);
+    }
+}
+
+public class M02ProgrammingAssignment1 {
+    public static void main(String[] args) {
+        MyDate date1 = new MyDate();
+        MyDate date2 = new MyDate(34355555133101L);
+
+        System.out.println("Date 1: " + date1.getYear() + "/" + (date1.getMonth() + 1) + "/" + date1.getDay());
+        System.out.println("Date 2: " + date2.getYear() + "/" + (date2.getMonth() + 1) + "/" + date2.getDay());
+    }
+}
