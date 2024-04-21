27dee0ffaea20929ec1644394f029ef740d84057
Rmaz01 <144160948+Rmaz01@users.noreply.github.com>
2024-04-21 01:01:28 -0500

    Add files via upload

---
+++ M05ProgrammingAssignment2.java
@@ -0,0 +1,37 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class M05ProgrammingAssignment2 extends Application {
+  @Override
+  public void start(Stage primaryStage) {
+   
+    StackPane pane = new StackPane();
+
+    // Create a circle and set its properties
+    Circle circle = new Circle(50);
+    circle.setFill(Color.WHITE);
+    circle.setStroke(Color.BLACK);
+    pane.getChildren().add(circle);
+
+  
+    pane.setOnMousePressed(e -> {
+      circle.setFill(Color.BLACK);
+    });
+
+    pane.setOnMouseReleased(e -> {
+      circle.setFill(Color.WHITE);
+      circle.setStroke(Color.BLACK);
+    });
+
+    
+    Scene scene = new Scene(pane, 120, 120);
+    primaryStage.setTitle("Exercise_15_07"); 
+    primaryStage.setScene(scene); 
+    primaryStage.show(); 
+
+  }
+}
